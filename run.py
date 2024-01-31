4c26f89719906dccc87b49138111e9d3e0bc8f02
Mohammed Motorwala <mmotorwala@juniper.net>
2024-01-30 18:13:10 -0800

    rename files from main to run

---
+++ run.py
@@ -0,0 +1,21 @@
+from func.kafka import kafka_get, kafka_put
+from func.processor import process_kafka_download, extract_meeting_options
+from func.io import get_rewind_count, get_speedup_factor, display_options
+
+
+# get inputs from user
+rewind_count = get_rewind_count()
+speedup_factor = get_speedup_factor()
+
+# get data from wh-call-qss-
+downloaded_data = kafka_get(rewind_count)
+
+# process kafka data
+processed_kafka_data = process_kafka_download(downloaded_data)
+meeting_options = extract_meeting_options(processed_kafka_data)
+
+# present options with stats
+choice = display_options(meeting_options)
+
+# poke data at intervals
+kafka_put(processed_kafka_data, meeting_options[choice][0], speedup_factor)
