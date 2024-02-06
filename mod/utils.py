4f50359ce3925bb40245ffede8d8721122331886
Mohammed Motorwala <mmotorwala@juniper.net>
2024-02-06 15:28:40 -0800

    consolidate local and nonlocal run

--- mod/utils.py
+++ mod/utils.py
@@ -2,6 +2,7 @@ import subprocess as _subprocess
 from collections import defaultdict as _collections_defaultdict
 from json import dumps as _json_dumps
 from json import loads as _json_loads
+from sys import argv as _sys_argv
 from time import sleep as _time_sleep
 
 from dateutil.parser import parse as _parse_dt
@@ -40,3 +41,19 @@ def json_loads(input: str) -> dict[str, object]:
 
 def create_defaultdict(func) -> _collections_defaultdict:
     return _collections_defaultdict(func)
+
+
+def parse_args() -> tuple[bool, str]:
+    argc = len(_sys_argv)
+    if argc < 2 or argc > 3 or (_sys_argv[1] != 'LOCAL' and _sys_argv[1] != 'NONLOCAL'):
+        print("USAGE: ./run.py (LOCAL|NONLOCAL) [dump_file_name]")
+        raise ValueError
+
+    return _sys_argv[1] == 'LOCAL', 'temp/dump.txt' if argc == 2 else _sys_argv[2]
+
+
+def local_get(filename: str) -> str:
+    local_data = None
+    with open(filename, 'r') as r:
+        local_data = r.read()
+    return local_data
--- run.py
+++ run.py
@@ -1,22 +1,35 @@
 #!/Users/mmotorwala/.pyenv/shims/python3
 
-from mod.io import display_options, get_rewind_count, get_speedup_factor
+from mod.io import (display_options, get_rewind_count,get_speedup_factor)
 from mod.kafka import kafka_get, kafka_put
 from mod.processor import extract_meeting_options, process_kafka_download
+from mod.utils import local_get, parse_args
 
-# get inputs from user
-rewind_count = get_rewind_count()
-speedup_factor = get_speedup_factor()
 
-# get data from wh-call-qss-
-downloaded_data = kafka_get(rewind_count)
+def main():
+    try:
+        is_local, dump_file_name = parse_args()
 
-# process kafka data
-processed_kafka_data = process_kafka_download(downloaded_data)
-meeting_options = extract_meeting_options(processed_kafka_data)
+        # get inputs from user (if needed) and retrieve data
+        speedup_factor = get_speedup_factor()
+        if is_local:
+            downloaded_data = local_get(dump_file_name)
+        else:
+            rewind_count = get_rewind_count()
+            downloaded_data = kafka_get(rewind_count)
 
-# present options with stats
-choice = display_options(meeting_options)
+        # process kafka data
+        processed_kafka_data = process_kafka_download(downloaded_data)
+        meeting_options = extract_meeting_options(processed_kafka_data)
 
-# poke data at intervals
-kafka_put(processed_kafka_data, meeting_options[choice][0], speedup_factor)
+        # present options with stats
+        choice = display_options(meeting_options)
+
+        # poke data at intervals
+        kafka_put(processed_kafka_data, meeting_options[choice][0], speedup_factor)
+    except ValueError:
+        return
+
+
+if __name__ == "__main__":
+    main()
